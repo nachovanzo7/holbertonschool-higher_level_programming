22a5aa415eb16aa842dfb354a74999aeb5cb3b42
Nacho Vanzo <9426@holbertonstudents.com>
2024-09-04 17:05:29 +0000

    Convert to hexadecimal

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    if (x >= 0 and x < 10):
-        print("0{}, ".format(x), end="")
-    elif(x == 99):
-        print("{}".format(x))
+    if(x == 99):
+        print("{:02}".format(x))
     else:
-        print("{}, ".format(x), end="")
+        print("{:02}, ".format(x), end="")
