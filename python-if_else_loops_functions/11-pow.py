bf606921ac79df5b9ffc262c9e2babddb1a9be9b
Nacho Vanzo <9426@holbertonstudents.com>
2024-09-05 20:52:51 +0000

    Exponential - Python

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 def pow(a, b):
-    sum = 0
-    for x in range(b):
-        sum = a * a
+    sum = 1
+
+    if b > 0:  
+        for x in range(b):
+            sum *= a
+    elif b == 0:  
+        return 1
+    else:  
+        for x in range(abs(b)):
+            sum *= a
+        sum = 1 / sum
+
     return sum
