062b310b9a28364ad90ad1d8de33bba8848196b0
Nacho Vanzo <9426@holbertonstudents.com>
2024-09-06 14:49:48 +0000

    Arguments from sys - Python

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 2:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for x in range(1, len(sys.argv)):
+            print("{}: {}".format((x), sys.argv[x]))
+    elif len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("1 argument:")
+        print("{}".format(sys.argv[1]))
