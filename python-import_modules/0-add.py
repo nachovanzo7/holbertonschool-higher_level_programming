7adb03b332b20e671cab1c514a09d871980ab870
Nacho Vanzo <9426@holbertonstudents.com>
2024-09-06 13:46:55 +0000

    Import add - Python

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as sum
+from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    res = sum.add(a, b)
+    res = add(a, b)
     print("{} + {} = {}".format(str(a), str(b), str(res)))
