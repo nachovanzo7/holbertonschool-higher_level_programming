9489ed065eeebae86323f81c21b5de92c4b88eb7
Nacho Vanzo <9426@holbertonstudents.com>
2024-09-12 23:11:23 +0000

    Lambda - Python

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    i = 0
-    if a_dictionary == None or not a_dictionary:
+    max = 0
+    if a_dictionary is None or not a_dictionary:
         return None
-    i = max(a_dictionary, key=a_dictionary.get)
-    return i
+    max = max(a_dictionary, key=a_dictionary.get)
+    return max
