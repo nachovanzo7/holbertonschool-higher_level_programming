1b2ae05bad692a271cab6d2630c92e06afa882d4
Nacho Vanzo <9426@holbertonstudents.com>
2024-09-23 18:24:47 +0000

    Attributes and methods from Object - Python

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -8,6 +8,16 @@ Parámetros:
 Retorna:
 - Una lista de nombres que representan los atributos y métodos disponibles del objeto proporcionado.
 """
+
 def lookup(obj):
+    """
+    Devuelve una lista de atributos y métodos disponibles para el objeto dado.
+
+    Parámetros:
+    - obj: El objeto cuyo conjunto de atributos y métodos se desea consultar.
+
+    Retorna:
+    - lista: Una lista de cadenas que representan los nombres de los atributos y métodos.
+    """
     lista = dir(obj)
     return lista
