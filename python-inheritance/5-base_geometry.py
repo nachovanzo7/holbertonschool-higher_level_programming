a594952a3750f8fbcbb513b533feac40b2f29574
Nacho Vanzo <9426@holbertonstudents.com>
2024-09-24 17:13:42 +0000

    Create a empty class - Python

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+
+"""
+Módulo que define la clase BaseGeometry.
+
+Este módulo contiene la clase BaseGeometry, que sirve como clase base
+para formas geométricas. Actualmente, es una clase vacía, pero puede ser
+extendida con métodos y atributos específicos en el futuro.
+"""
+
 class BaseGeometry:
+    """
+    Clase base para formas geométricas.
+
+    Esta clase está diseñada para ser una clase base de la cual se pueden 
+    derivar otras clases que representen formas geométricas. Actualmente,
+    no contiene métodos ni atributos.
+    """
     pass
