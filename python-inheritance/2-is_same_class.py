5ff34eb4cdde0bdb5c1c019ce839b8715716561b
Nacho Vanzo <9426@holbertonstudents.com>
2024-09-24 16:17:37 +0000

    Compare type of class - Python

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
+"""
+Este módulo contiene una función que verifica si un objeto es exactamente
+una instancia de una clase especificada.
+
+Funciones:
+    is_same_class(obj, a_class): Retorna True si el objeto es exactamente
+    una instancia de la clase especificada, de lo contrario retorna False.
+"""
+
 def is_same_class(obj, a_class):
-    if (type(obj) == a_class):
+    """
+    Verifica si el objeto 'obj' es exactamente una instancia de la clase 'a_class'.
+    
+    Args:
+        obj: El objeto a verificar.
+        a_class: La clase contra la que se compara el objeto.
+    
+    Returns:
+        True si 'obj' es exactamente una instancia de 'a_class',
+        False en cualquier otro caso (incluidas subclases).
+    """
+    if type(obj) == a_class:
         return True
     else:
         return False
