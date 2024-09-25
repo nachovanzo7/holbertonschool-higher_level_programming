971aeb64f63149de1c5af466b42f3f995ec7e11c
Nacho Vanzo <9426@holbertonstudents.com>
2024-09-25 13:43:26 +0000

    Instantiation - Python

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+Módulo que define la clase BaseGeometry.
+
+Este módulo contiene la clase BaseGeometry, que sirve como clase base
+para formas geométricas. Actualmente, es una clase vacía, pero puede ser
+extendida con métodos y atributos específicos en el futuro.
+"""
+
+class BaseGeometry:
+    """
+    Clase base para formas geométricas.
+
+    Esta clase está diseñada para ser una clase base de la cual se pueden 
+    derivar otras clases que representen formas geométricas. Actualmente,
+    no contiene métodos ni atributos.
+    """
+
+    def area(self):
+        if (self.area):
+            raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Clase que representa un rectángulo, hereda de BaseGeometry.
+    
+    Args:
+        width (int): Ancho del rectángulo.
+        height (int): Altura del rectángulo.
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
