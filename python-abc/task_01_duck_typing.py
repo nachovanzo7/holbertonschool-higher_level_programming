0993bca35b43cd2424ff4fb026fa60aa428d2f17
Nacho Vanzo <9426@holbertonstudents.com>
2024-09-28 20:22:35 +0000

    Using abstract method with calculation - Python

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter():
+        pass
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (self.width + self.height) * 2
+
+def shape_info(shape):
+    print("Area: {}\nPerimeter: {}".format(shape.area(), shape.perimeter()))
