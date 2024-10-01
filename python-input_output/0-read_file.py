80883a18fab7b13adc2f3b48131b803ad4242c04
Nacho Vanzo <9426@holbertonstudents.com>
2024-10-01 22:07:46 +0000

    Read files - Python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""
+Este módulo contiene una función para leer el contenido de un archivo de texto y
+mostrarlo en la salida estándar.
+"""
 
 def read_file(filename=""):
+    """
+    Lee el contenido de un archivo de texto y lo imprime en la salida estándar.
+
+    Args:
+        filename (str): Nombre del archivo que se va a leer. El valor predeterminado
+        es una cadena vacía, lo que significa que se debe especificar un archivo.
+
+    Raises:
+        FileNotFoundError: Si el archivo especificado no existe o no se puede encontrar.
+        UnicodeDecodeError: Si ocurre un error al decodificar el contenido del archivo.
+    """
     with open(filename, "r", encoding="utf-8") as archivo:
         texto = archivo.read()
         print(texto)
