5d3ae093a366cc0b6f0194a37563b399d70c0f1c
Nacho Vanzo <9426@holbertonstudents.com>
2024-10-02 17:51:48 +0000

    Load file - Python

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,6 +2,22 @@
 import json
 
 def load_from_json_file(filename):
+    """
+    Carga un objeto desde un archivo JSON.
+
+    Args:
+        filename (str): El nombre del archivo JSON desde el cual se va a cargar el objeto.
+
+    Returns:
+        objeto: El objeto Python representado por los datos JSON contenidos en el archivo.
+    
+    Excepciones:
+        FileNotFoundError: Si el archivo no existe.
+        json.JSONDecodeError: Si el contenido del archivo no es un JSON válido.
+    
+    Ejemplo:
+        objeto = load_from_json_file("archivo.json")
+    """
     with open(filename, 'r', encoding='utf-8') as archivo:
-        objeto = json.load(archivo)
+        objeto = json.load(archivo)  # Carga el contenido JSON desde el archivo
         return objeto
