d0a1b5a62abd864b061fecec142a8f00f7e67abd
Nacho Vanzo <9426@holbertonstudents.com>
2024-10-02 18:47:37 +0000

    Add item - Python

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Este script agrega todos los argumentos pasados por línea de comandos
+a una lista, y luego guarda dicha lista en un archivo JSON llamado 'add_item.json'.
+Si el archivo ya existe, carga su contenido previamente para agregar los nuevos
+elementos. Si no existe, crea el archivo.
+"""
+
+import sys
+import json
+from os import path  # Correccion chat-gpt
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+if path.exists(filename):  # Si el archivo existe - carga
+    item = load_from_json_file(filename)
+else:
+    item = []  # Caso de que no exista - creacion
+
+item.extend(sys.argv[1:])  # Recibo argumentos del usuario y concateno
+
+save_to_json_file(item, filename)
