225c5d3a9cb60ea4b6093117595af2ec6e431665
Nacho Vanzo <9426@holbertonstudents.com>
2024-10-06 23:04:57 +0000

    XML - Python

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -2,20 +2,6 @@
 
 import xml.etree.ElementTree as ET
 
-def serialize_to_xml(dictionary, filename):
-    # Crear el elemento raíz <data>
-    root = ET.Element('data')
-    
-    # Iterar a través de los elementos del diccionario
-    for key, value in dictionary.items():
-        # Crear un elemento hijo para cada par clave-valor
-        item = ET.SubElement(root, 'item', key=key)
-        item.text = str(value)  # Convertir el valor a cadena
-    
-    # Crear el árbol XML y guardarlo en el archivo proporcionado
-    tree = ET.ElementTree(root)
-    tree.write(filename, encoding='utf-8', xml_declaration=True)
-
 def deserialize_from_xml(filename):
     # Parsear el archivo XML
     tree = ET.parse(filename)
@@ -34,7 +20,7 @@ def deserialize_from_xml(filename):
             try:
                 value = float(value)
             except ValueError:
-                pass
+                pass  # Mantener como cadena si no es un número
         
         dictionary[key] = value
     
