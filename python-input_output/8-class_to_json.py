5e3572b2f73c5960ccb07a5a4d993810488ae3fa
Nacho Vanzo <9426@holbertonstudents.com>
2024-10-06 14:23:15 +0000

    Class to JSON (fix document) - Python

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+Este módulo proporciona una función para convertir los atributos de un objeto a un diccionario.
+"""
 
 def class_to_json(obj):
+    """
+    Parámetros:
+        obj (object): La instancia de la clase cuyo contenido se convertirá a un diccionario.
+    
+    Retorno:
+        dict: Un diccionario que contiene los atributos del objeto.
+    """
     return obj.__dict__
