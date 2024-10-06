f8811c968b3c0ef975020e414da066cafd962dcb
Nacho Vanzo <9426@holbertonstudents.com>
2024-10-06 16:19:47 +0000

    Pascal triangle - Python

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+def pascal_triangle(n):
+    """
+    Genera el Triángulo de Pascal hasta la fila n.
+
+    Parámetros:
+        n (int): El número de filas del Triángulo de Pascal a generar.
+                  Si n es menor o igual a 0, devuelve una lista vacía.
+
+    Retorno:
+        list: Una lista que contiene las filas del Triángulo de Pascal, 
+               donde cada fila es una lista de enteros.
+    """
+    list = []
+    if n <= 0:
+        return list
+
+    for i in range(n):
+        fila = [1] * (i + 1)
+
+        for x in range(1, i): #menos las puntas, se suman los nums anteriores
+            fila[x] = (list[i - 1][x - 1]) + (list[i - 1][x])
+
+        list.append(fila)
+
+    return list
