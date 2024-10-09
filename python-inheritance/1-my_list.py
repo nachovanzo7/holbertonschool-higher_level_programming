dc6c8a372ed9fb339622af2e3a683bd0e64f5c2c
Nacho Vanzo <9426@holbertonstudents.com>
2024-10-09 16:06:46 +0000

    Print a list - Pyhon

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Modulo para utilizar python
+"""
+
 
 class MyList(list):
+    """
+    Herencia de list e imprime una lista
+    """
     def print_sorted(self):
         print(sorted(self))
