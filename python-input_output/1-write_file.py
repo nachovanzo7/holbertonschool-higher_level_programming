99621cda810c13db33c60f1c6a1aec9b8b892217
Nacho Vanzo <9426@holbertonstudents.com>
2024-10-09 14:51:14 +0000

    Write File documented - Python

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-
+"""
+Modulo para utilizar Python
+"""
 def write_file(filename="", text=""):
+    """
+    Funcion que escribe dentro de un archivo
+    y retorna la cantidad de caracteres escritas
+    """
     if (filename is not None):
         with open(filename, "w", encoding="utf-8") as archivo:
             len = archivo.write(text)
