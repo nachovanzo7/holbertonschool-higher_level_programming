56e58f242fecdd28d6b28bd4277f70d4c6e41e74
Nacho Vanzo <9426@holbertonstudents.com>
2024-10-30 22:10:44 +0000

    JOIN ON - SQL

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Listar todas las ciudades en la base de datos
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = state_id
+JOIN states ON states.id = state_id
 ORDER BY cities.id ASC;
