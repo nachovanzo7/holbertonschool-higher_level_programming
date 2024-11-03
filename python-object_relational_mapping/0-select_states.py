643353739c2b6e1d9ad8db5ebcad45b06f975f5f
Nacho Vanzo <9426@holbertonstudents.com>
2024-11-03 21:44:17 +0000

    Trying connect to database - SQL

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    usuario = sys.argv[1]
+    password = sys.argv[2]
+    datos = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=usuario,
+        passwd=password,
+        db=datos
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
