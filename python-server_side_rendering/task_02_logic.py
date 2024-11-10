2cafd90ca98f2f68f5189001561e34a5243b3a1c
root <root@H08.hbtn.uy>
2024-11-10 18:12:11 -0300

    Flask Logic - Python

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as file:
+        items_data = json.load(file)
+    return render_template('items.html', items=items_data["items"])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+    
