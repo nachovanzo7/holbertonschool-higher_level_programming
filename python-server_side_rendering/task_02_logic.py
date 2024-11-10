8a50cdcd4e1eb62b983c1da2e1bf5f222c175711
root <root@H08.hbtn.uy>
2024-11-10 18:34:01 -0300

    Status code correction - FLask

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def items():
     with open('items.json', 'r') as file:
         items_data = json.load(file)
-    return render_template('items.html', items=items_data["items"])
+    return render_template('items.html', items=items_data["items"]), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
