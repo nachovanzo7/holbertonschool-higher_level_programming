7f0d4970595ca22353e6d95214613e0a4974961d
root <root@H08.hbtn.uy>
2024-11-10 20:54:29 -0300

    SQL problems - Flask

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -8,16 +8,23 @@ app = Flask(__name__)
 @app.route('/products')
 def products():
     source = request.args.get('source')
+    product_id = request.args.get('id')
     
     data = []
 
     try:
-        if source == 'sql': #--> Los datos se sacan de sql
+        if source == 'sql':  # Los datos se sacan de sql
             conn = sqlite3.connect('products.db')
             cursor = conn.cursor()
-            cursor.execute('SELECT * FROM Products')
+
+            if product_id:
+                cursor.execute('SELECT * FROM Products WHERE id = ?', (product_id,))
+            else:
+                cursor.execute('SELECT * FROM Products')
             rows = cursor.fetchall()
             conn.close()
+            
+            
             for row in rows:
                 product = {
                     'id': row[0],
@@ -27,20 +34,26 @@ def products():
                 }
                 data.append(product)
 
-        elif source == 'json': #--> Los datos se sacan de json
+        elif source == 'json':  # Los datos se sacan de json
             with open('products.json') as file:
                 data = json.load(file)
+                # Filtramos por ID si se especifica
+                if product_id:
+                    data = [product for product in data if str(product['id']) == product_id]
 
-        elif source == 'csv': #--> Los datos se sacan de csv
+        elif source == 'csv':  # Los datos se sacan de csv
             with open('products.csv') as file:
                 reader = csv.DictReader(file)
                 data = [row for row in reader]
+                # Filtramos por ID si se especifica
+                if product_id:
+                    data = [product for product in data if product['id'] == product_id]
 
         else:
             return render_template('product_display.html', error="Wrong source"), 200
 
     except sqlite3.Error as e:
-        return render_template('product_display.html', error="Database error: {}".format(e)), 200
+        return render_template('product_display.html', error="Database error: {}".format(e)), 500
     except Exception as e:
         return render_template('product_display.html', error="Error: {}".format(e)), 500
 
