ff1627cd2c44eb822ecbef5d26b55f227ce5cbbe
root <root@H08.hbtn.uy>
2024-11-10 19:43:32 -0300

    Fixing data from JSON and CSV - Flask

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -4,7 +4,7 @@ import csv
 
 app = Flask(__name__)
 
-@app.route('/data')
+@app.route('/products')
 def get_data():
     source = request.args.get('source')
     item_id = request.args.get('id', type=int)
@@ -16,14 +16,24 @@ def get_data():
 
     if source == 'json': #Si es json
         try:
-            with open('data.json', 'r') as file:
+            with open('products.json', 'r') as file:
                 data = json.load(file)
 
+        except FileNotFoundError:
+            return render_template('product_display.html', error="JSON not found")
+        except json.JSONDecodeError:
+            return render_template('product_display.html', error="ERROR --> JSON File")
+        
     elif source == 'csv': #Si es csv
         try:
-            with open('data.csv', 'r') as file:
+            with open('products.csv', 'r') as file:
                 reader = csv.DictReader(file)
                 data = [row for row in reader]
+                
+        except FileNotFoundError:
+            return render_template('product_display.html', error="CSV not found")
+        except csv.Error:
+            return render_template('product_display.html', error="ERROR --> CSV File")
 
     # Filtrar por 'id' si se proporciona
     if item_id is not None:
